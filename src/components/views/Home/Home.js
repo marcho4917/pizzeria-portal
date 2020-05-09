6c594b254f7074421587962500d11a1ba2469477
Martyna Chojnowska <martynachojnowska12@gmail.com>
2020-05-09 13:35:19 +0200

    actualization

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,20 +1,91 @@
 import React from 'react';
-import styles from './Home.module.scss';
-import { Link } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-const Home = () => {
-  return(
-    <div className={styles.component}>
-      <div>
-        <h1>Log in to start working:</h1>
-      </div>
-      <TextField id="standard-basic" label="login" /><br />
-      <TextField id="standard-basic" label="Password" /><br />
-      <Button variant="contained" component={Link} to={'/panel/login/'}>START WORK!</Button>
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
 };
 
-export default Home;
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ScrollableTabsButtonAuto() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="Local orders" {...a11yProps(0)} />
+          <Tab label="Take away orders" {...a11yProps(1)} />
+          <Tab label="Reservations today" {...a11yProps(2)} />
+          <Tab label="Events today" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        56
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        184
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        
+      </TabPanel>
+    </div>
+  );
+}
--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,52 +1,24 @@
 import React from 'react';
 import styles from './Login.module.scss';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
-const demoContent = [
-  {id: 'Monday', status: 'free', amount: '-'},
-  {id: 'Tuesday', status: '6', amount: 44},
-  {id: 'Wednesday', status: '12', amount: 12},
-  {id: 'Thursday', status: '8.5', amount: 32},
-  {id: 'Friday', status: '4', amount: 35},
-  {id: 'Saturday', status: 'free', amount: '-'},
-  {id: 'Sunday', status: 'free', amount: '-'},
-];
-
-const Login = () => (
-  <Paper className={styles.component}>
-    <div>
-      <h3>Thats your weekly working time and the number of orders youve completed</h3>
+const Login = () => {
+  return(
+    <div className={styles.component}>
+      <Box display="flex" justifyContent="center">
+        <TextField id="standard-basic" label="login" /> 
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <TextField id="standard-basic" label="Password" />
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <Button variant="contained" component={Link} to={process.env.PUBLIC_URL + '/'}>START WORK!</Button>
+      </Box>
     </div>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Week day</TableCell>
-          <TableCell>Hours</TableCell>
-          <TableCell>Number of orders</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {demoContent.map(row => (
-          <TableRow key={row.id}>
-            <TableCell component="th" scope="row">
-              {row.id}
-            </TableCell>
-            <TableCell>
-              {row.status}
-            </TableCell>
-            <TableCell>
-              {row.amount}
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </Paper>
-);
+  );
+};
 
 export default Login;
